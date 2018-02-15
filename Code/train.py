35f835f4dbe73922bd82b1324641438326a96679
gokzs <gokulav2@gmail.com>
2018-02-15 08:56:13 +0530

    improved acc

--- Code/train.py
+++ Code/train.py
@@ -1,52 +1,78 @@
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense,Dropout,Activation
+from keras.layers import Dense,Dropout,Conv2D,MaxPool2D, Flatten
 from keras.utils import np_utils
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.optimizers import RMSprop
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
 
 a = pd.read_csv("../Data/train.csv")
 b = pd.read_csv("../Data/test.csv")
 
-print (a.shape)
+# print (a.shape)
 train_Y = pd.DataFrame()
 train_Y["label"] = a["label"]
 
 # preprocessing
 train_X = a.iloc[:,1:].values.astype('float')
 test_X = b.values.astype('float')
-max = np.max(train_X)
-train_X /= max
-test_X /= max
+max = 255
+train_X /= 255
+test_X /= 255
 
-mean = np.std(train_X)
-train_X -= mean
-test_X -= mean
+train_X = train_X.reshape(-1, 28, 28, 1)
+test_X = test_X.reshape(-1, 28, 28, 1)
+
+# mean = np.std(train_X)
+# train_X -= mean
+# test_X -= mean
 # print(train_X)
 
 # to binary matrix
 train_Y = np_utils.to_categorical(train_Y["label"]).astype('int')
 
+# print(train_Y.shape)
 
-train_dim = train_X.shape[1]
-no_classes = train_Y.shape[1]
 
-# print(train_X)
-# print(train_Y)
+np.random.seed(2)
+random_seed = 2
+
+train_X, train_test_X, train_Y, train_test_Y = train_test_split(train_X, train_Y, test_size=0.1, random_state=random_seed)
+
+# g = plt.imshow(train_X[0][:, :, 0])
+
+print(train_X.shape)
+print(train_Y.shape)
 model = Sequential()
 
-model.add(Dense(units=500, input_dim=train_dim))
-model.add(Activation('relu'))
-model.add(Dropout(0.1))
-model.add(Dense(units=300))
-model.add(Activation('relu'))
-model.add(Dropout(0.1))
-model.add(Dense(no_classes))
-model.add(Activation("softmax"))
+model.add(Conv2D(32,(5, 5), activation='relu', input_shape=(28,28,1)))
+model.add(Conv2D(32,(5, 5), activation='relu'))
+model.add(MaxPool2D(2,2))
+model.add(Dropout(0.25))
 
-model.compile(loss = 'categorical_crossentropy', optimizer = 'Adam', metrics=['accuracy'])
-fit = model.fit(train_X, train_Y, epochs=10, batch_size=16, validation_split=0.1,verbose=2 )
+model.add(Conv2D(64,(3, 3), activation='relu'))
+model.add(Conv2D(64,(3, 3), activation='relu'))
+model.add(MaxPool2D(2,2))
+model.add(Dropout(0.25))
 
+model.add(Flatten())
+model.add(Dense(units=256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units=10, activation='softmax'))
+
+model.compile(loss = 'categorical_crossentropy', optimizer = RMSprop(lr=0.001), metrics=['accuracy'])
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
+                                            patience=3,
+                                            verbose=1,
+                                            factor=0.5,
+                                            min_lr=0.00001)
+datagen = ImageDataGenerator()
+fit = model.fit_generator(datagen.flow(train_X, train_Y, batch_size=96), epochs=1,
+                          validation_data=(train_test_X, train_test_Y), steps_per_epoch=train_X.shape[0],
+                          callbacks = [learning_rate_reduction])
 plt.plot(fit.history['acc'])
 plt.plot(fit.history['val_acc'])
 plt.title('Model Accuracy')
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Kaggle competition project 
 
-Accuracy of 96.68% on Kaggle.
+Accuracy of 98.93% on Kaggle.
 
 Simple feed-forward Structure with no hidden layers.
